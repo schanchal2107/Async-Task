3a98bf93f47cc6d819095261a569476a2441e525
schanchal2107 <chanchal.sharma@cs.christuniversity.in>
2021-04-04 16:09:36 +0530

    Initial Commi

---
+++ app/src/main/java/com/example/mycardview/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.mycardview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    ImageView imageView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        imageView = (ImageView) findViewById(R.id.imageView);
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, start.class);
+                //Intent is used to switch from one activity to another.
+
+                startActivity(i);
+                //invoke the SecondActivity.
+
+                finish();
+            }
+        });
+    }
+}
